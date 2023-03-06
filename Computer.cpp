ef184584d7327199ba0c5464cead8d3828205469
natchong <100551804+natchong@users.noreply.github.com>
2023-03-06 19:52:28 +1030

    Add files via upload

---
+++ Computer.cpp
@@ -0,0 +1,14 @@
+#include "Computer.h"
+using namespace std;
+
+//default constructor
+Computer::Computer(){
+    name = "Computer";
+    move = '\0';
+}
+
+
+char Computer::playerMove(){
+    char move = 'R';
+    return move;
+}
