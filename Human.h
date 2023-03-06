a41236d23592d87db9a0d56e5ca361ec48681e13
natchong <100551804+natchong@users.noreply.github.com>
2023-03-06 20:09:35 +1030

    Add files via upload

---
+++ Human.h
@@ -0,0 +1,15 @@
+#ifndef HUMAN_H
+#define HUMAN_H
+
+#include "Player.h"
+
+
+class Human : public Player{
+
+    public:
+    Human(); // default constructor
+    Human(std::string playerName);
+    char playerMove();
+};
+
+#endif
