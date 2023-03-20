6045f2769f3d13742e6ba26e7c730ea0ec35dd02
natchong <100551804+natchong@users.noreply.github.com>
2023-03-20 16:12:55 +1030

    new

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,18 +1,28 @@
 #include "Reverser.h"
 using namespace std;
 
-int Reverser::reverseDigit(int value){ 
-    return reverseDigitRecursive(value, 0);
-}
-
-int Reverser:: reverseDigitRecursive(int value, int reversedValue){
-    if (value==0){
-        return reversedValue;
+int Reverser::reverseDigit(int value){
+    if(value < 10){
+        return value;
+    }
+    int remainder = value % 10;
+    int quotient = value / 10;
+    int reversed = reverseDigit(quotient);
+    int numOfDigits = 1 ;
+    int temp = quotient;
+    while (temp > 0 ){
+        numOfDigits *= 10;
+        temp /= 10;
     }
 
+    return remainder * numOfDigits + reversed;
+}
 
-int last = value % 10;
-reversedValue = reversedValue * 10 + last;
-
-return reverseDigitRecursive(value/10, reversedValue);
+std::string Reverser::reverseString(std::string characters){
+    if(characters.length() == 1){
+        return characters;
+    } 
+    char first = characters[0];
+    std::string rest = characters.substr(1);
+    return reverseString(rest) + first;
 }
--- main2.cpp
+++ main2.cpp
@@ -1,18 +1,18 @@
-#include "Reverser.cpp"
+#include "Reverser.h"
 #include <iostream>
 using namespace std;
 
 int main(){
     Reverser r;
 
-    int value= 12345;
-    int reversedValue= r.reverseDigit(value);
-    std::cout << "Reverse of " << value << "is" << reversedValue <<std::endl;
+    int value = 54321;
+    int reversedValue = r.reverseDigit(value);
+    std::cout << "Reverse value: " << reversedValue << std::endl;
 
     std::string characters = "natasha";
-    std::string reversedCharacters = r.reverseString(characters);
-    std::cout << "Reverse of " << characters << "is" << reversedCharacters <<std::endl;
-
+    std::string reversedString = r.reverseString(characters);
+    std::cout << "Reverse string: " << reversedString << std::endl;
     return 0;
+
 
 }
