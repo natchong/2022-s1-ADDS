f04783e080373517b8d6accfbcef1704439219f9
natchong <100551804+natchong@users.noreply.github.com>
2023-03-20 16:34:26 +1030

    new

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,21 +1,22 @@
 #include "Reverser.h"
 using namespace std;
+#include <string>
 
 int Reverser::reverseDigit(int value){
     if(value < 10){
         return value;
     }
-    int remainder = value % 10;
-    int quotient = value / 10;
-    int reversed = reverseDigit(quotient);
+    int a = value % 10;
+    int b = value / 10;
+    int reversed = reverseDigit(b);
     int numOfDigits = 1 ;
-    int temp = quotient;
+    int temp = b;
     while (temp > 0 ){
         numOfDigits *= 10;
         temp /= 10;
     }
 
-    return remainder * numOfDigits + reversed;
+    return a * numOfDigits + reversed;
 }
 
 std::string Reverser::reverseString(std::string characters){
@@ -23,6 +24,6 @@ std::string Reverser::reverseString(std::string characters){
         return characters;
     } 
     char first = characters[0];
-    std::string rest = characters.substr(1);
-    return reverseString(rest) + first;
+    std::string others = characters.substr(1);
+    return reverseString(others) + first;
 }
