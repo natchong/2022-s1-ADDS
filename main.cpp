20d2e0f417acf2f7c25b4a6105f2d22970a020f0
natchong <100551804+natchong@users.noreply.github.com>
2023-03-20 16:00:24 +1030

    new

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "Truckloads.h"
 #include <iostream>
 using namespace std;
+#include <iostream>
 
 int main(){
     int numCrates, loadSize;
-    std::cout << "Number of crates: "; 
+
+    std::cout << "Number of crates: " ;
     std::cin >> numCrates;
-    std::cout << "Max load size: ";
+    std::cout << "Max load size: " ;
     std::cin >> loadSize;
 
     Truckloads truckloads;
