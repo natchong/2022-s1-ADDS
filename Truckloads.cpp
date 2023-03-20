1e2a8777986ad40c21538d084f16ce4c5913bdb7
natchong <100551804+natchong@users.noreply.github.com>
2023-03-20 15:59:56 +1030

    new

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -1,12 +1,28 @@
 #include "Truckloads.h"
+using namespace std;
+#include <iostream> 
+
 
 int Truckloads::numTrucks(int numCrates, int loadSize){
-    if(numCrates < 2 || numCrates >10000 || loadSize < 1 || loadSize > (numCrates-1)){
-        return 0;
-    }else 
-    if (numCrates <= loadSize){ 
+
+    int count;
+
+    if(numCrates==0 || loadSize ==0){
+        return 0; 
+    }
+    if (numCrates <= loadSize){
         return 1;
     }
-    int half = numCrates/2; 
-    return numTrucks(half, loadSize) + numTrucks(numCrates-half, loadSize);
-}
+
+    if(numCrates %2 != 0){
+        int first_pile = numCrates/2;
+        int second_pile = numCrates - first_pile;
+        return numTrucks(first_pile, loadSize) + numTrucks(second_pile, loadSize);
+    }
+
+    else{
+        numCrates = numCrates/2;
+        count ++;
+    }
+    return numTrucks(numCrates, loadSize) + numTrucks(numCrates, loadSize);
+} 
