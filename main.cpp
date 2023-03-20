6c2e7245174a3954b848121c4dc65179ee48376d
natchong <100551804+natchong@users.noreply.github.com>
2023-03-20 14:26:28 +1030

    new

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "Truckloads.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numCrates, loadSize;
+    std::cout << "Number of crates: "; 
+    std::cin >> numCrates;
+    std::cout << "Max load size: ";
+    std::cin >> loadSize;
+
+    Truckloads truckloads;
+    int numTrucks= truckloads.numTrucks(numCrates, loadSize);
+    std::cout << "Number of trucks needed: " << numTrucks << std::endl;
+
+    return 0;
+}
