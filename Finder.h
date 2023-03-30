87f2706eed1ea527140436a1ac0b057dbd0f61f5
natchong <100551804+natchong@users.noreply.github.com>
2023-03-30 18:39:09 +1030

    Add files via upload

---
+++ Finder.h
@@ -0,0 +1,14 @@
+#ifndef FINDER_H
+#define FINDER_H
+
+#include <string>
+#include <vector>
+
+
+class Finder {
+    public:
+    std::vector<int> findSubstrings(std::string s1, std::string s2);
+
+};
+
+#endif
