65009f776e4390071345c447e8f8532968bedb73
natchong <100551804+natchong@users.noreply.github.com>
2023-04-03 23:18:26 +0930

    Add files via upload

--- Finder.cpp
+++ Finder.cpp
@@ -12,9 +12,9 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
         size_t found = s1.find(s2.substr(0, i));
         if (found != string::npos) {
             result.push_back(found);
+            return result;
             } else {
              result.push_back(-1);
-             i++;
             }
     }
                 return result;
