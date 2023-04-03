76fe4c5739fe41b4057db6f2c3db9cd6251ff2fe
natchong <100551804+natchong@users.noreply.github.com>
2023-04-03 23:14:03 +0930

    Add files via upload

--- Finder.cpp
+++ Finder.cpp
@@ -1,21 +1,21 @@
 #include "Finder.h"
 #include <vector>
 #include <string>
+
 using namespace std;
 
-vector<int> Finder::findSubstrings(string s1, string s2){
+vector<int> Finder::findSubstrings(string s1, string s2) {
+
     vector<int> result;
 
-    for (size_t i=1; i<=s2.size(); i++){
-        size_t found = s1.find(s2.substr(0,i));
-        
-        if(found != string::npos){
+    for(size_t i = 1; i <= s2.size(); i++) {
+        size_t found = s1.find(s2.substr(0, i));
+        if (found != string::npos) {
             result.push_back(found);
-        } else {
-            result.push_back(-1);
-            result.push_back(-1);
-            i++;
-        }
+            } else {
+             result.push_back(-1);
+             i++;
+            }
     }
-    return result;
+                return result;
 }
